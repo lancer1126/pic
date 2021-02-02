439dea7c37da0506006160a4e388eedbb3380859
lancer1126 <lancer1582@gmail.com>
2021-02-02 19:02:23 +0800

    BaseLayout add

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import BaseLayout from '../layouts/BaseLayout';
+import MainLayout from '../layouts/MainLayout';
+
+import Home from '../views/Home'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    component: BaseLayout,
+    children: [
+      {
+        path: '/',
+        component: MainLayout,
+        props: {safeArea: true},
+        children: [
+          {
+            path: '/',
+            redirect: '/home'
+          },
+          {
+            path: '/home',
+            name: 'Home',
+            component: Home
+          }
+        ]
+      }
+    ]
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
